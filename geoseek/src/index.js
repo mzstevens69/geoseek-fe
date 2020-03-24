595be597a8e6718e41ad287d293ba13bc4d91a39
Michael Z Stevens <54222870+mzstevens69@users.noreply.github.com>
2020-03-24 08:05:59 -0500

    Update index.js

--- geoseek/src/index.js
+++ geoseek/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppRedux from './AppRedux';
 import * as serviceWorker from './serviceWorker';
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import {Provider} from "react-redux";
+import {BrowserRouter as Router} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import {createStore, applyMiddleware} from "redux";
+import {gemReducer} from './reducer/gemReducer';
+
+const store = createStore(gemReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <AppRedux/>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
